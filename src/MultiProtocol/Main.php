fb10e35b59e32e124e38675c24159498338a7448
Lil Kev <kjolly544@gmail.com>
2024-04-11 00:40:56 -0400

    Who know if this will work

--- src/MultiProtocol/Main.php
+++ src/MultiProtocol/Main.php
@@ -5,35 +5,36 @@ namespace MultiProtocol;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\utils\Config;
-use pocketmine\network\mcpe\protocol\ProtocolInfo;
 use pocketmine\event\server\DataPacketReceiveEvent;
-use pocketmine\network\mcpe\protocol\LoginPacket;
+use pocketmine\network\mcpe\handler\LoginPacketHandler;
 
 class Main extends PluginBase implements Listener {
-	
-   public $acceptProtocol = [];
-
-   public function onEnable(): void {
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->getLogger()->info("MultiProtocol Plugin Enable!");
-		
-		@mkdir($this->getDataFolder());
-		$this->acceptProtocol = (new Config($this->getDataFolder()."accept.yml", Config::YAML))->get("accept-protocol");
-		
-		if ($this->acceptProtocol === false || empty($this->acceptProtocol)) {
-			$this->acceptProtocol[] = ProtocolInfo::CURRENT_PROTOCOL;
-			$config = new Config($this->getDataFolder()."accept.yml", Config::YAML);
-			$config->set("accept-protocol", [ProtocolInfo::CURRENT_PROTOCOL]);
-			$config->save();
-		}
-    }
     
-    public function onDataPacketReceive(DataPacketReceiveEvent $ev): void {
-    	$pk = $ev->getPacket();
-    	if ($pk instanceof LoginPacket) {
-    		if (in_array($pk->protocol, $this->acceptProtocol)) {
-    			$pk->protocol = ProtocolInfo::CURRENT_PROTOCOL;
-    		}
-    	}
+    /** @var array */
+    private $acceptProtocol;
+
+    public function onEnable(): void {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info("MultiProtocol Plugin Enabled!");
+
+        @mkdir($this->getDataFolder());
+        $config = new Config($this->getDataFolder() . "accept.yml", Config::YAML);
+        $this->acceptProtocol = $config->get("accept-protocol", []);
+        if (empty($this->acceptProtocol)) {
+            $this->acceptProtocol[] = $this->getServer()->getVersion()->getProtocol();
+            $config->set("accept-protocol", $this->acceptProtocol);
+            $config->save();
+        }
+    }
+
+    public function onDataPacketReceive(DataPacketReceiveEvent $event): void {
+        $packet = $event->getPacket();
+        if ($packet instanceof LoginPacket) {
+            $handler = new LoginPacketHandler($packet);
+            if (!in_array($handler->protocol, $this->acceptProtocol)) {
+                $handler->protocol = $this->getServer()->getVersion()->getProtocol();
+                $event->setPacket($handler->getPacket());
+            }
+        }
     }
 }
